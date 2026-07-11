5b72e69cb5f432d25c5215b2ba84f7e0c368250b
rahulkumar-live <95327083+rahulkumar-live@users.noreply.github.com>
2026-07-11 15:36:56 +0530

    Day 3

--- Solve Problems on Arrays/Easy/removeDuplicates.java
+++ Solve Problems on Arrays/Easy/removeDuplicates.java
@@ -2,10 +2,10 @@ public class removeDuplicates {
     public static void main(String[] args) {
         removeDuplicates obj = new removeDuplicates();
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int result = obj.removeDuplicates(nums);
+        int result = obj.removeDuplicate(nums);
         System.out.println(result); // Output: 5
     }
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicate(int[] nums) {
         int right = 0, left = nums.length - 1, result = 0;
         while(right < left){
             if(nums[right] == nums[right + 1]){
