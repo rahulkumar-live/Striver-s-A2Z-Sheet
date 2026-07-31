3d99d85bdb6bf8cfca5022f790afe9ee7d0047ab
rahulkumar-live <95327083+rahulkumar-live@users.noreply.github.com>
2026-07-31 23:40:51 +0530

    new Question

---
+++ Pratyush Sheet/Two Pointers/BackSpace.java
@@ -0,0 +1,46 @@
+public class BackSpace {
+    public static void main(String[] args) {
+        BackSpace obj = new BackSpace();
+        String s = "ab#c", t = "ad#c";
+        boolean result = obj.backspaceCompare(s, t);
+        System.out.println(result); // Output: true
+    }
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1, j = t.length() - 1;
+        while(i >= 0 || j >= 0){
+            int skipS = 0;
+            while (i >= 0) {
+                if(s.charAt(i) == '#'){
+                    skipS++;
+                    i--;
+                }
+                else if(skipS > 0){
+                    skipS--;
+                    i--;
+                } 
+                else
+                    break;
+            }
+            int skipT = 0;
+            while(j >= 0){
+                if(t.charAt(j) == '#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT > 0){
+                    skipT--;
+                    j--;
+                }
+                else
+                    break;
+            }
+            if((i >= 0) != (j >= 0))
+                return false;
+            if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j))
+                return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
